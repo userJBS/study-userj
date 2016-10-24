73b1b964e15f5cf6b5636e19328f0ba23758aaa1
jbs-PC\jbs <zxcvb4825@naver.com>
2016-10-24 22:16:05 +0900

    2-3 완료

--- src/main/java/com/userj/web/UserController.java
+++ src/main/java/com/userj/web/UserController.java
@@ -1,15 +1,28 @@
 package com.userj.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	private List<User> users = new ArrayList<User>();
 
 	@PostMapping("/create")
 	public String create(User user){
 		System.out.println(user);
-		return "index";
+		users.add(user);
+		return "redirect:/list"; //회원가입후 list로 이동
 	}
 	
+	@GetMapping("/list")
+	public String list(Model model){
+		model.addAttribute("users", users);
+		return "list";
+	}
+
 }
