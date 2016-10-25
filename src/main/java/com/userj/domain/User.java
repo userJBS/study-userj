6b070f8e629e6987543b271048b1e58baa09ea93
jbs <jbs@jbs-PC>
2016-10-26 01:43:29 +0900

    3-2 완료 객체와 테이블 매핑

---
+++ src/main/java/com/userj/domain/User.java
@@ -0,0 +1,30 @@
+package com.userj.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter //( 롬복 셋팅이 필요)
+@ToString 
+@Entity // 데이터 베이스와 매핑하게된다.
+public class User {
+
+	@Id // primary key키설정 (고유값이 된다.)
+	@GeneratedValue // 자동으로 값을 1씩 증가시켜준다.
+	private Long id;
+
+	@Column(length = 20) // 최대 길이 20으로 설정
+	@NotBlank // null, "", "(space)" 세가지 조건 값들을 허용하지 않는다.
+	private String userId;
+	private String password;
+	private String name;
+	private String email;
+
+	// setter.. toString 부분  
+}
--- src/main/java/com/userj/web/UserController.java
+++ src/main/java/com/userj/web/UserController.java
@@ -3,25 +3,32 @@ package com.userj.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.userj.domain.User;
+import com.userj.domain.UserRepository;
+
 @Controller
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@PostMapping("/create")
-	public String create(User user){
+	public String create(User user) {
 		System.out.println(user);
-		users.add(user);
-		return "redirect:/list"; //회원가입후 list로 이동
+		userRepository.save(user); // 받아온 값들을 DB에 저장.
+		return "redirect:/list"; // 회원가입후 list로 이동
 	}
-	
+
 	@GetMapping("/list")
-	public String list(Model model){
-		model.addAttribute("users", users);
+	public String list(Model model) {
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 
