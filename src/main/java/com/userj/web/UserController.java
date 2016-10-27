3e578235dfcd997fe40af8d867643e73a62f45b0
jbs <jbs@jbs-PC>
2016-10-27 16:01:14 +0900

    [3-3] 새로운 HTML 적용 및 URL 리팩토링 완료

--- src/main/java/com/userj/web/UserController.java
+++ src/main/java/com/userj/web/UserController.java
@@ -8,28 +8,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.userj.domain.User;
 import com.userj.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 
 	@Autowired
 	private UserRepository userRepository;
 
-	@PostMapping("/create")
+	@GetMapping("/form")
+	public String form(){
+		return "/user/form";
+	}
+	
+	@PostMapping
 	public String create(User user) {
 		System.out.println(user);
 		userRepository.save(user); // 받아온 값들을 DB에 저장.
-		return "redirect:/list"; // 회원가입후 list로 이동
+		return "redirect:/users"; // users URL로 이동
 	}
+	
 
-	@GetMapping("/list")
+	@GetMapping
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
-		return "list";
+		return "/user/list";  // templates/user/list.html 뷰
 	}
 
 }
