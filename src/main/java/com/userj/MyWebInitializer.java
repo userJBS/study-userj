7cfb0af59692af4a28078ce7e86186066cb2c58d
Administrator <Administrator@smart20111>
2016-11-01 14:50:18 +0900

    4-6. 네 번째 반복주기 원격 서버 배포 외부 톰캣 사용을 위한 설정

---
+++ src/main/java/com/userj/MyWebInitializer.java
@@ -0,0 +1,13 @@
+package com.userj;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class MyWebInitializer extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyWebInitializer.class);
+	}
+
+}
