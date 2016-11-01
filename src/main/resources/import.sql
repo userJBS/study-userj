84e8a7f803409a59f0fca6c2b09ca907a0b8045a
Administrator <Administrator@smart20111>
2016-11-01 09:32:25 +0900

    테스트

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('id01', 'pass01', '계정', 'email01@naver.com');
+insert into USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('id01', 'pass01', 'account01', 'email01@naver.com');
 
